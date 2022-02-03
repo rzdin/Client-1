3d425639f706ab0a54facb0b38cd358b2cca7a9a
Rana Ziauddin <96728892+rzdin@users.noreply.github.com>
2022-02-03 18:04:52 +0500

    edited jenkinsfile

--- JenkinFilesng.groovy
+++ JenkinFilesng.groovy
@@ -30,6 +30,7 @@ pipeline {
 
                 sh "npm install -g npm"
                 sh "npm install -g @angular/cli"
+                sh "npm run ng"
                 sh "ng build --prod"
                 echo "***********LINE***********"
                 echo "**********LINE2***********"
